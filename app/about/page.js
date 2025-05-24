5aa3dfea551909737d4e4b93b5b784607b70f2a9
Shento Hendriks <shentohendriks@gmail.com>
2025-05-24 13:39:14 +0200

    Synced by Front Matter

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="container mx-auto px-4">
+      <h1 className="text-4xl mb-4">About me</h1>
+      <p>My name is Shento, I'm a front-end developer!</p>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,17 +1,32 @@
 import fs from "fs";
+import matter from "gray-matter";
 import Link from "next/link";
 
 export default function Home() {
-  const posts = fs.readdirSync("posts").map((f) => f.replace(".mdx", ""));
+  const posts = fs
+    .readdirSync("posts")
+    .map((file) => {
+      const slug = file.replace(".mdx", "");
+      const { data } = matter(fs.readFileSync(`posts/${file}`, "utf8"));
+      return { slug, data };
+    })
+    .sort((a, b) => new Date(b.data.date) - new Date(a.data.date)); // Sort by date, newest first
 
   return (
     <div className="container mx-auto p-4">
-      <h1>My Blog</h1>
-      {posts.map((post) => (
-        <div key={post}>
-          <Link href={`/${post}`}>{post}</Link>
-        </div>
-      ))}
+      <h1 className="text-4xl mb-4">Blog</h1>
+      <div className="flex flex-col gap-2">
+        {posts.map(({ slug, data }) => (
+          <article key={slug}>
+            <Link
+              className="font-bold"
+              href={`/${slug}`}>
+              {data.title}
+            </Link>
+            <p className="text-gray-400">{data.date}</p>
+          </article>
+        ))}
+      </div>
     </div>
   );
 }
