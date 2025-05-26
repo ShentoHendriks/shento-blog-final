a491c3b01c8bdfd3222774fa421e17b7a9a932c8
Shento Hendriks <shentohendriks@gmail.com>
2025-05-27 00:37:44 +0200

    Added File Structure Component

---
+++ components/FileStructure.jsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+export function FileStructure({ children }) {
+  return (
+    <div className="bg-[#293056] p-10 flex justify-center items-center rounded-md flex-col my-[3rem]">
+      <div>{children}</div>
+    </div>
+  );
+}
+
+export function File({ children, level = 0, type = "file", focus = false }) {
+  if (level == 0) {
+    level = 14;
+  } else {
+    level = 14 + level * 28;
+  }
+  return (
+    <div
+      style={{ paddingLeft: level + "px" }}
+      className={`bg-white relative first-of-type:rounded-t-md pr-8 ${
+        focus && "font-bold"
+      } first-of-type:border-b-[#D5D9EB]
+    border last-of-type:border-t-[#D5D9EB] p-3.5 border-y-[#D5D9EB] border-[0.5px] last-of-type:rounded-b-md min-w-[250px]`}>
+      <div className="flex items-center justify-start gap-3">
+        <img
+          className="border-none w-4 h-4 m-0"
+          src={`${type == "file" ? "file.svg" : "folder.svg"}`}
+          alt="Icon"
+        />
+        <p className="m-0">{children}</p>
+        {focus && (
+          <span className="w-2 h-2 rounded-full absolute right-5 bg-[#B3B8DB]"></span>
+        )}
+      </div>
+    </div>
+  );
+}
--- components/index.js
+++ components/index.js
@@ -2,13 +2,16 @@ import Button from "@/components/Button";
 import { Tabs, TabPanel } from "./Tabs";
 import Callout from "./Callout";
 import { InteractivePlayground } from "./InteractivePlayground";
+import {FileStructure, File} from "./FileStructure";
 
 const components = {
   Button,
   Tabs,
   TabPanel,
   Callout,
-  InteractivePlayground
+  InteractivePlayground,
+  FileStructure,
+  File
 };
 
 export default components;
