132499907eb3303352dfd529948cc302e60c5877
Shento Hendriks <shentohendriks@gmail.com>
2025-05-29 13:47:33 +0200

    Restyled the callout component. Made fonts smaller inside.

--- components/Callout.jsx
+++ components/Callout.jsx
@@ -1,6 +1,6 @@
 export default function Callout({ children }) {
   return (
-    <div className="rounded-md border border-[#D5D9EB] px-6 py-4 my-[2em] callout">
+    <div className="rounded-md border bg-[#EAECF5] border-[#D5D9EB] px-6 py-4 my-[2em] callout text-sm">
       {children}
     </div>
   );
