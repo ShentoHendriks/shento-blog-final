2630952480b81067ff4067911d881f6cc9b5430e
Shento Hendriks <shentohendriks@gmail.com>
2025-05-29 14:54:41 +0200

    Removed background border on callouts, and added better contrast on filestructure component

--- components/Callout.jsx
+++ components/Callout.jsx
@@ -1,6 +1,24 @@
-export default function Callout({ children }) {
+export default function Callout({ children, type = "info" }) {
+  const typeStyles = {
+    danger: {
+      bg: "bg-[#FEE4E2]",
+    },
+    warning: {
+      bg: "bg-[#FEF0C7]",
+    },
+    success: {
+      bg: "bg-[#DCFAE6]",
+    },
+    info: {
+      bg: "bg-[#EAECF5]",
+    },
+  };
+
+  const styles = typeStyles[type] || typeStyles.info;
+
   return (
-    <div className="rounded-md border bg-[#EAECF5] border-[#D5D9EB] px-6 py-4 my-[2em] callout text-sm">
+    <div
+      className={`rounded-md ${styles.bg} ${styles.text} px-6 py-4 my-[2em] callout text-sm`}>
       {children}
     </div>
   );
