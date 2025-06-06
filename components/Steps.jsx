d0f72899fe0b549cdc90cc5ac05d091d205e8a22
Shento Hendriks <shentohendriks@gmail.com>
2025-06-06 13:04:45 +0200

    added more spacing in steps (margin)

--- components/Steps.jsx
+++ components/Steps.jsx
@@ -9,8 +9,8 @@ export function Steps({ children, width = "800px", vertical }) {
   const style = width ? { "--custom-width": width } : {};
 
   return (
-    <div
-      className={`my-4 ${
+    <section
+      className={`mb-4 mt-16 flow-root ${
         width
           ? "lg:w-[calc(var(--custom-width)_-_2em)] lg:max-w-[calc(100vw_-_2em)] lg:mx-auto lg:relative lg:left-1/2 lg:-translate-x-1/2"
           : ""
@@ -24,7 +24,7 @@ export function Steps({ children, width = "800px", vertical }) {
           ? cloneElement(step, { stepNumber: index + 1 })
           : step
       )}
-    </div>
+    </section>
   );
 }
 
