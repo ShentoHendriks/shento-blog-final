94c7c9fc7707dec8bf4c111cec9e569efaefb557
Shento Hendriks <shentohendriks@gmail.com>
2025-06-06 02:00:58 +0200

    searches-now-through-content

--- app/page.js
+++ app/page.js
@@ -2,22 +2,20 @@ import fs from "fs";
 import matter from "gray-matter";
 import BlogWithSearch from "./BlogWithSearch";
 
-// TODO: create a component that allows selecting posts and shows next and previous post (for related tutorials)
-
 function getPosts() {
   return fs
     .readdirSync("posts")
     .map((file) => {
       const slug = file.replace(".mdx", "");
-      const { data } = matter(fs.readFileSync(`posts/${file}`, "utf8"));
-      return { slug, data };
+      const fileContent = fs.readFileSync(`posts/${file}`, "utf8");
+      const { data, content } = matter(fileContent);
+      return { slug, data, content };
     })
     .sort((a, b) => new Date(b.data.date) - new Date(a.data.date));
 }
 
 export default function Home() {
   const posts = getPosts();
-
   return (
     <div className="container mx-auto p-4">
       <h1>Articles</h1>
